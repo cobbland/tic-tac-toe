f75e9ce8bfec6cd856479fed1a48e70755bb8d80
cobbland <jacobdensford@proton.me>
2025-02-15 09:11:13 -0500

    Make working visual game
    Rewrote the game logic into smaller chunks so I could
    interact with them via the webapp.

--- js/scipts.js
+++ js/scipts.js
@@ -16,6 +16,8 @@ const GameBoard = (function() {
         }
     }
 
+    makeBoard();
+
     function markPlace(player, row, column, board = gameBoard) {
         if (board[row] === undefined || board[row][column] === undefined || board[row][column] !== 0) {
             return false;
@@ -49,10 +51,10 @@ const GameBoard = (function() {
             gameBoard[2][0] === gameBoard[2][1] && gameBoard[2][1] === gameBoard[2][2]) ||
             (gameBoard[0][0] + gameBoard[1][0] + gameBoard[2][0] !== 0 &&
             gameBoard[0][0] === gameBoard[1][0] && gameBoard[1][0] === gameBoard[2][0]) ||
-            (gameBoard[1][0] + gameBoard[1][1] + gameBoard[1][2] !== 0 &&
-            gameBoard[1][0] === gameBoard[1][1] && gameBoard[1][1] === gameBoard[1][2]) ||
-            (gameBoard[2][0] + gameBoard[2][1] + gameBoard[2][2] !== 0 &&
-            gameBoard[2][0] === gameBoard[2][1] && gameBoard[2][1] === gameBoard[2][2]) ||
+            (gameBoard[0][1] + gameBoard[1][1] + gameBoard[2][1] !== 0 &&
+            gameBoard[0][1] === gameBoard[1][1] && gameBoard[1][1] === gameBoard[2][1]) ||
+            (gameBoard[0][2] + gameBoard[1][2] + gameBoard[2][2] !== 0 &&
+            gameBoard[0][2] === gameBoard[1][2] && gameBoard[1][2] === gameBoard[2][2]) ||
             (gameBoard[0][0] + gameBoard[1][1] + gameBoard[2][2] !== 0 &&
             gameBoard[0][0] === gameBoard[1][1] && gameBoard[1][1] === gameBoard[2][2]) ||
             (gameBoard[2][0] + gameBoard[1][1] + gameBoard[0][2] !== 0 &&
@@ -76,26 +78,35 @@ const Players = (function() {
         player2 = {name: second, mark: 'O'};
     }
 
+    makePlayers();
+
     return {player1, player2, makePlayers};
 })();
 
 const PlayGame = (function() {
-    const players = Players;
+    const players = [Players.player1, Players.player2];
     const gameBoard = GameBoard;
 
     let currentPlayer = players[0];
     let winningPlayer = gameOver();
 
-    gameBoard.makeBoard();
+    function newGame() {
+        gameBoard.makeBoard();
+        currentPlayer = players[0];
+        winningPlayer = gameOver();
+    }
 
     function playTurn(player = currentPlayer, row, column) {
         currentPlayer = player;
-        gameBoard.markPlace(player, row, column);
-        if (gameOver()) {
-            winningPlayer = currentPlayer;
-        } else if (gameOver() === 'Tie!') {
-            winningPlayer = 'Tie!'
-        } else if (currentPlayer = players[0]) {
+        if (gameBoard.markPlace(player, row, column)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function nextPlayer() {
+        if (currentPlayer === players[0]) {
             currentPlayer = players[1];
         } else {
             currentPlayer = players[0];
@@ -112,46 +123,17 @@ const PlayGame = (function() {
 
     function gameOver() {
         if (gameBoard.checkWinner()) {
+            winningPlayer = currentPlayer;
             return true;
         } else if (!gameBoard.checkWinner()) {
             return false;
         } else {
-            return 'Tie!'
+            winningPlayer = 'Tie!';
+            return 'Tie!';
         }
     }
 
-    function playGame() {
-        let winningPlayer;
-        while (winner === false) {
-            for (let player in players) {
-                while (!gameBoard.markPlace(
-                    players[player],
-                    +prompt(`${players[player].name}, in what row would you like to place your ${players[player].mark}?`), 
-                    +prompt(`${players[player].name}, in what column would you like to place your ${players[player].mark}?`)
-                ));
-                gameBoard.seeBoard();
-                winner = gameBoard.checkWinner();
-                if (winner === true) {
-                    winningPlayer = players[player].name;
-                    break;
-                } else if (winner !== false) {
-                    break;
-                }
-            }
-        }
-        if (winner === true) {
-            console.log(`We have a winner! ${winningPlayer} wins!`);
-        } else if (winner === "Tie!") {
-            console.log(winner);
-        }
-        console.log(winner);
-        gameBoard.makeBoard();
-        winner = gameBoard.checkWinner();
-    }
-
-    return {playGame, playTurn, checkCurrentPlayer, checkWinningPlayer};
-
-    // displayController (later)
+    return {playTurn, checkCurrentPlayer, checkWinningPlayer, newGame, nextPlayer, gameOver};
 
 })();
 
@@ -160,10 +142,13 @@ const displayGame = (function() {
     const display = document.querySelector('.display');
     const buttons = document.querySelector('.buttons');
     
-    const gameBoard = GameBoard.seeBoard();
     const boardFunctions = GameBoard;
-    
-    display.textContent = "Player 1's turn..."
+    const playFunctions = PlayGame;
+    const playerFunctions = Players;
+
+    let gameBoard = boardFunctions.seeBoard();
+
+    let gameOn = false;
 
     function drawBoard() {
         while (board.firstChild) {
@@ -175,7 +160,9 @@ const displayGame = (function() {
                 thisCell.classList.add('cell');
                 thisCell.setAttribute('row', row);
                 thisCell.setAttribute('column', cell);
-                thisCell.textContent = gameBoard[row][cell];
+                if (gameBoard[row][cell] !== 0) {
+                    thisCell.textContent = gameBoard[row][cell];
+                }
                 board.appendChild(thisCell);
             }
         }
@@ -184,15 +171,35 @@ const displayGame = (function() {
     drawBoard();
 
     board.addEventListener('click', (button) => {
-        let row = button.target.getAttribute('row');
-        let column = button.target.getAttribute('column');
-        gameBoard[row][column] = 'X';
-        drawBoard();
+        if (gameOn) {
+            let row = button.target.getAttribute('row');
+            let column = button.target.getAttribute('column');
+            let player = playFunctions.checkCurrentPlayer();
+            if (playFunctions.playTurn(player, row, column)) {
+                drawBoard();
+                if (playFunctions.gameOver() !== false) {
+                    console.log('the game is over...')
+                    display.textContent = `${playFunctions.checkWinningPlayer().name} wins!`;
+                    gameOn = false;
+                } else {
+                    playFunctions.nextPlayer();
+                    player = playFunctions.checkCurrentPlayer();
+                    display.textContent = `${player.name}'s turn...`;
+                }
+            } else {
+                display.textContent = "Can't play there. Try again.";
+            }
+        }
     });
 
-    buttons.addEventListener('click', () => {
-        boardFunctions.makeBoard();
-        drawBoard();
+    buttons.addEventListener('click', (button) => {
+        if (button.target.classList.contains('start')) {
+            gameOn = true;
+            playFunctions.newGame();
+            gameBoard = boardFunctions.seeBoard();
+            drawBoard();
+            display.textContent = `${playFunctions.checkCurrentPlayer().name}'s turn...`;
+        } 
     })
 
 })
